9fa3d2ccce42a1e59eab25d2a9c1ea365af68a41
nianmobai <2819827128@qq.com>
2023-02-13 22:25:58 +0800

    add codes

--- blogPage/func.js
+++ blogPage/func.js
@@ -9,3 +9,34 @@ $(document).ready(function () {
         $('#blogbody').height(view_areaHeight - blog_bodyPosition['top']);
     }
 })
+$(document).ready(function () {
+    $('.Navigate-button').hover(
+        function () {
+            $(this).css('background-color', 'rgba(0, 0, 0, 0.4)');
+        },
+        function () {
+            $(this).css('background-color', 'rgba(255, 255, 255, 0.8)');
+        }
+    )
+})
+
+/**
+ * function: check if  the path is exist 
+ * @param {string} url the path of a file
+ * @returns true if the path is exist ,else return false
+ */
+function FileExist(url) {
+    let exixst;
+    $.ajax({
+        type: 'HEAD',
+        url: url,
+        async: false,
+        success: function () {
+            exixst = true;
+        },
+        error: function () {
+            exixst = false;
+        }
+    })
+    return exixst;
+}
