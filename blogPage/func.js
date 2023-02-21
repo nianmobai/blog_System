929abdfa4ab2734d3cb0c623193f20a9845de4c4
nianmobai <2819827128@qq.com>
2023-02-21 16:32:51 +0800

    change-element typesetting

--- blogPage/func.js
+++ blogPage/func.js
@@ -1,16 +1,19 @@
 const Retrieval_model = '<div class="article-Retrieval border flex"><div class="article-pic border" ><img></div><div class="article-Intro flex-direction-column flex border"><div class="headline flex border"></div><div class="briefIntro border"></div><div class="time border flex"></div></div></div> ';
 const first_child_Element = '>.article-Retrieval:eq{0}';
+const tag_model = '<div class="tag-label"></div>'
 console.log("导入成功");
-$(document).ready(function () {
+$(document).ready(Adjust_Size());
+function Adjust_Size() {
     let blog_bodyPosition = $('#blogbody').offset();
     let blog_bodyHeight = $('#blogbody').height();
     console.log(blog_bodyHeight)
     console.log(blog_bodyPosition['top']);
-    let view_areaHeight = $(window).height()
-    if ((blog_bodyHeight + blog_bodyPosition['top']) < view_areaHeight) {
+    let view_areaHeight = $(document).height()
+    if ((blog_bodyHeight + blog_bodyPosition['top']) != view_areaHeight) {
         $('#blogbody').height(view_areaHeight - blog_bodyPosition['top']);
+        $('#mainPart').height(blog_bodyPosition['top'] + $('#blogbody').height());
     }
-})
+}
 $(document).ready(function () {
     $('.Navigate-button').hover(
         function () {
@@ -29,6 +32,8 @@ $('.Navigate-button').bind('click', function () {
     $('#content > div').hide();
     $('#' + content).fadeIn(200);
 })
+
+//Get_ArticleIntro();
 
 /**
  * function: check if  the path is exist 
@@ -50,6 +55,36 @@ function FileExist(url) {
     })
     return exist;
 }
+
+/**
+ * function: add the entance to login, also add a special effect to headpic
+ */
+$('#headpic').hover(
+    function () {
+        $(this).css('transform', 'scale(1.02)');
+    },
+    function () {
+        $(this).css('transform', 'scale(1)');
+    }).bind('click', function () {
+        window.location.href = '../login/signIn.html';
+        $.ajax({
+            url: '../index.php',
+            async: false,
+            /*test:
+            sucess: function (data) {
+                console.log(data);
+            },*/
+            error: function () {
+                console.log('link error')
+            }
+        })
+    }).mousedown(function () {
+        $(this).css('transform', 'scale(1.01)');
+    }).mouseup(
+        function () {
+            $(this).css('transform', 'scale(1.02)');
+        })
+
 /**
  * function: receive an object of an article with all the data including sort,headline and so on
  * @param {object} article an object with all the data of article
@@ -70,6 +105,7 @@ function Get_ArticleIntro() {
     $.ajax({
         url: '',
         type: 'POST',
+        async: false,
         dataType: 'json',
         success: function (data, sttus) {
             let result = JSON.parse(data);
