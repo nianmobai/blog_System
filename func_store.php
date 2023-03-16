149bff4462f9187bc7da2f0a351eddc748726565
nianmobai <112251517+nianmobai@users.noreply.github.com>
2023-03-16 23:02:37 +0800

    little change

--- func_store.php
+++ func_store.php
@@ -1,1 +1,35 @@
 <?php
+function Get_BlogHosterInro()
+{
+}
+class Login
+{
+    private $password = null;
+    private $accountNum = null;
+    private $host = 'localhost';
+    private $dbname_HosterIntro = '';
+    private $db_password = '';
+
+    public function __construct()
+    {
+        $this->accountNum = Get_Account();
+        $this->password = Get_Pssword();
+    }
+    public function Comfirm($ps, $ac)
+    {
+
+        if ($ac == $this->accountNum && $ps == $this > password) {
+            //realize Page Turn
+        } else {
+            //throw error
+        }
+    }
+    private function Get_Account()
+    {
+        return;
+    }
+    private function Get_Pssword()
+    {
+        return;
+    }
+}
