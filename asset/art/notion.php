b641bc6a4e1db7fde38904664dff58bc552b4601
nianmobai <112251517+nianmobai@users.noreply.github.com>
2023-07-08 20:45:18 +0800

    little change(nothing)

--- asset/art/notion.php
+++ asset/art/notion.php
@@ -1,2 +1,14 @@
 <?php
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>文章不存在</p>
+</body>
+</html>
